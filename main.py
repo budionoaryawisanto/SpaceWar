7760a92fded2b413e25030e33edef3302530bb58
budionoaryawisanto <budionoarya.w@gmail.com>
2022-11-02 15:03:03 +0700

    progres player musuh dan shoot metode

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from pygame.locals import *
 
 pygame.init()
 
+
+player_ship = 'playership.png'
+enemy_ship = 'enemyship1.png'
+meteor = 'meteor.png'
+player_bullet = 'pbullet.png'
+enemy_bullet = 'ebullet.png'
+
+
 screen = pygame.display.set_mode((0,0), FULLSCREEN)
 s_width, s_height = screen.get_size()
 
@@ -12,6 +20,12 @@ clock = pygame.time.Clock()
 FPS = 60
 
 background_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
+playerbullet_group = pygame.sprite.Group()
+enemybullet_group = pygame.sprite.Group()
+
 sprite_group = pygame.sprite.Group()
 
 class Background(pygame.sprite.Sprite):
@@ -30,12 +44,90 @@ class Background(pygame.sprite.Sprite):
             self.rect.y = random.randrange(-10, 0)
             self.rect.x = random.randrange(-400, s_width)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, img):
+        super().__init__()
+        self.image = pygame.image.load(img) 
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey('black')
+
+    def update(self):
+        mouse = pygame.mouse.get_pos()
+        self.rect.x = mouse[0] - 9
+        self.rect.y = mouse[1] - 5
+
+    def shoot(self):
+        bullet = PlayerBullet(player_bullet)
+        mouse = pygame.mouse.get_pos()
+        bullet.rect.x = mouse[0]
+        bullet.rect.y = mouse[1]
+        playerbullet_group.add(bullet)
+        sprite_group.add(bullet)
+
+class Enemy(Player):
+    def __init__(self, img):
+        super().__init__(img)
+        self.rect.x = random.randrange(0, s_width)
+        self.rect.y = random.randrange(-500, 0)
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self):
+        self.rect.y += 1
+        if self.rect.y > s_height:
+            self.rect.x = random.randrange(0, s_width)
+            self.rect.y = random.randrange(-2000, 0)
+        self.shoot()
+
+    def shoot(self):
+        if self.rect.y in (0, 30, 40, 50, 200, 300, 700):
+            enemybullet = EnemyBullet(enemy_bullet)
+            enemybullet.rect.x = self.rect.x + 28
+            enemybullet.rect.y = self.rect.y + 20
+            enemybullet_group.add(enemybullet)
+            sprite_group.add(enemybullet)
+
+
+class Meteor(Enemy):
+    def __init__(self, img):
+        super().__init__(img)
+        self.rect.x = -200
+        self.rect.y = random.randrange(200, 500)
+        self.move = 1
+
+    def update(self):
+        self.rect.x += self.move
+        if self.rect.x > s_width + 200:
+            self.move *= -1
+        elif self.rect.x < -200:
+            self.move *= -1
+
+class PlayerBullet(pygame.sprite.Sprite):
+    def __init__(self, img):
+        super().__init__()
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey('black')
+
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.y < 0:
+            self.kill()
+
+class EnemyBullet(PlayerBullet):
+    def __init__(self, img):
+        super().__init__(img)
+
+    def update(self):
+        self.rect.y += 3
+        if self.rect.y > s_height:
+            self.kill()
+
 class Game: 
     def __init__(self):
         self.run_game()
 
     def create_background(self):
-        for i in range(15):
+        for i in range(30):
             x = random.randint(1,3)
             background_image = Background(x,x)
             background_image.rect.x = random.randrange(0, s_width)
@@ -43,12 +135,32 @@ class Game:
             background_group.add(background_image)
             sprite_group.add(background_image)
 
+    def create_player(self):
+        self.player = Player(player_ship)
+        player_group.add(self.player)
+        sprite_group.add(self.player)
+
+    def create_enemy(self):
+        for i in range(10):
+            self.enemy = Enemy(enemy_ship)
+            enemy_group.add(self.enemy)
+            sprite_group.add(self.enemy)
+
+    def create_meteor(self):
+        for i in range(1):
+            self.meteor = Meteor(meteor)
+            meteor_group.add(self.meteor)
+            sprite_group.add(self.meteor)
+
     def run_update(self):
         sprite_group.draw(screen)
         sprite_group.update()
 
     def run_game(self):
         self.create_background()
+        self.create_player()
+        self.create_enemy()
+        self.create_meteor()
         while True:
             screen.fill('black')
             self.run_update()
@@ -58,6 +170,7 @@ class Game:
                     sys.exit()
 
                 if event.type == KEYDOWN:
+                    self.player.shoot()
                     if event.key == K_ESCAPE:
                         pygame.quit()
                         sys.exit()
